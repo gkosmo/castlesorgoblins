9b032b54d28ad3361f3d77e08dc56e40dae00b07
george <gkosmo1@hotmail.com>
2017-08-18 18:05:29 +0200

    button to checkout games created

--- client/template/home/home.js
+++ client/template/home/home.js
@@ -1,14 +1,27 @@
 Template.home.onCreated(function(){
-    this.createOn = new ReactiveVar(false);
+    var self = this;
+    self.createOn = new ReactiveVar(false);
+    self.userDMgames  = new ReactiveVar(false);
+    self.autorun(function(){
+      self.subscribe('userDMgames');
+    });
 });
 Template.home.events({
   "click #create": function(event, template){
-    console.log(template);
      Template.instance().createOn.get() ? Template.instance().createOn.set(false) : Template.instance().createOn.set(true);
+  },
+  "click #userDMgames":function(event, template){
+    Template.instance().userDMgames.get() ? Template.instance().userDMgames.set(false) : Template.instance().userDMgames.set(true);
   }
 });
 Template.home.helpers({
   createOn: function(){
     return Template.instance().createOn.get();
+  },
+  userDMgamesOn: function(){
+    return Template.instance().userDMgames.get();
+  },
+  userDMgames: function(){
+    return Game.find({creatorId: Meteor.userId()});
   }
 });
