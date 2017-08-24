2db5d24ef652a60a6456620ed2d94a16ac0af61a
george <gkosmo1@hotmail.com>
2017-08-24 17:33:35 +0200

    where we are so far

---
+++ server/methods/newgame.js
@@ -0,0 +1,13 @@
+Meteor.methods({
+  'createGame':function(doc){
+    console.log("hey hey enter creategame");
+     check(doc.name, String);
+     check(doc.description, String);
+     for(var i = 0; i < doc.attributeList.length; i ++){
+       check(doc.attributeList[i], String);
+     }
+     doc[creatorId] = this.userId;
+     doc.members= [];
+     return Game.insert(doc);
+  }
+});
