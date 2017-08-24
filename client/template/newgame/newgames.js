5f3a7815e1bb8417bd8a0e2c9e142fb9de8dae53
george <gkosmo1@hotmail.com>
2017-08-24 18:36:03 +0200

    can create games

--- client/template/newgame/newgames.js
+++ client/template/newgame/newgames.js
@@ -12,6 +12,23 @@ Template.newgame.events({
       console.log($('#attributeName').val());
       console.log(attrList);
       return Template.instance().attributesPlayers.set(attrList);
+  },
+  "click #createGame":function(event, template){
+    var game = {}
+    game.name = $('#gameName').val();
+    game.description= $('#gameDescription').val();
+    game.private = template.find('#privateGame').checked;
+    game.attributeList = Template.instance().attributesPlayers.get();
+    game.members = [];
+    console.log(game);
+    Meteor.call("createGame",game, function(error, result){
+      if(error){
+        console.log("error", error);
+      }
+      if(result){
+         console.log("yey");
+      }
+    });
   }
 });
 Template.newgame.helpers({
--- server/methods/newgame.js
+++ server/methods/newgame.js
@@ -6,9 +6,9 @@ Meteor.methods({
      for(var i = 0; i < doc.attributeList.length; i ++){
        check(doc.attributeList[i], String);
      }
-     doc[creatorId] = this.userId;
+     doc.creatorId = this.userId;
      doc.members= [];
      return Game.insert(doc);
   }
-  
+
 });
