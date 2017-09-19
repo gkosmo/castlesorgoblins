8b5d62403785fbb7e5f828d3370dcbcc2ce69b3a
george <gkosmo1@hotmail.com>
2017-09-19 18:10:10 +0200

    correction de push

--- server/publications/listgames.js
+++ server/publications/listgames.js
@@ -1,3 +1,6 @@
 Meteor.publish("userPLgames", function(){
    return PlayerGameList.find({playerId: this.userId});
 });
+Meteor.publish("userPLAgames", function(argument){
+  return Game.find({});
+});
